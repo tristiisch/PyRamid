3645405dd96f81740e0b366662b93324d0e46f3c
Tristiisch <tristiisch74@hotmail.com>
2023-10-24 07:07:59 +0200

    fix ci git_info.json, change version to 0.1.3

--- src/tools/information.py
+++ src/tools/information.py
@@ -9,7 +9,7 @@ class ProgramInformation:
 	def __init__(self):
 		self.name = "pyramid"
 		self.os = get_os().lower()
-		self.version = "0.1.2"
+		self.version = "0.1.3"
 		self.git_info = GitInfo()
 
 	def load_git_info(self):
