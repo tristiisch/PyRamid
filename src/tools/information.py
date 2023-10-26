384a0fd2a93e2350b4a5cf48cccac0306d003319
Tristiisch <tristiisch74@hotmail.com>
2023-10-26 05:04:30 +0200

    release v0.1.4

--- src/tools/information.py
+++ src/tools/information.py
@@ -9,7 +9,7 @@ class ProgramInformation:
 	def __init__(self):
 		self.name = "pyramid"
 		self.os = get_os().lower()
-		self.version = "0.1.3"
+		self.version = "0.1.4"
 		self.git_info = GitInfo()
 
 	def load_git_info(self):
