dfcf2f80acd918a4fc4fa676832c025433ebd013
Tristiisch <tristiisch74@hotmail.com>
2024-09-23 00:37:04 +0200

    feat: remove logger in cli, remove unused --health

--- src/cli.py
+++ src/cli.py
@@ -5,33 +5,33 @@ from pyramid.client.client import SocketClient
 from pyramid.client.requests.health import HealthRequest
 from pyramid.tools.logs_handler import LogsHandler
 
-info = ApplicationInfo()
+def startup_cli():
+	info = ApplicationInfo()
 
-logs_handler = LogsHandler()
-logs_handler.log_to_console()
+	parser = argparse.ArgumentParser(description="Readme at https://github.com/tristiisch/PyRamid")
+	parser.add_argument("--version", action="store_true", help="Print version", required=False)
 
-parser = argparse.ArgumentParser(description="Readme at https://github.com/tristiisch/PyRamid")
-parser.add_argument("--version", action="store_true", help="Print version", required=False)
-parser.add_argument("--health", action="store_true", help="Print health", required=False)
+	health_subparser = parser.add_subparsers(dest="health")
+	health_parser = health_subparser.add_parser("health", help="Check health status")
+	health_parser.add_argument(
+		"--host", type=str, help="Specify the host for health check", required=False
+	)
+	health_parser.add_argument(
+		"--port", type=int, help="Specify the port for health check", required=False
+	)
 
-health_subparser = parser.add_subparsers(dest="health")
-health_parser = health_subparser.add_parser("health", help="Print health")
-health_parser.add_argument(
-	"--host", type=str, help="Specify the host for health check", required=False
-)
-health_parser.add_argument(
-	"--port", type=int, help="Specify the port for health check", required=False
-)
+	args = parser.parse_args()
 
-args = parser.parse_args()
+	if args.version:
+		print(info.get_version())
 
-if args.version:
-	print(info.get_version())
+	elif args.health:
+		sc = SocketClient(args.host, args.port)
+		health = HealthRequest()
+		sc.send(health)
 
-elif args.health:
-	sc = SocketClient(args.host, args.port)
-	health = HealthRequest()
-	sc.send(health)
+	else:
+		parser.print_help()
 
-else:
-	parser.print_help()
+if __name__ == "__main__":
+	startup_cli()
