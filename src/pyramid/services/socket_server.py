ba78c1b050449b99cd2d23f459f6c36e93dd55c7
Tristiisch <tristiisch74@hotmail.com>
2024-12-06 00:05:59 +0100

    debug: socket server

--- src/pyramid/services/socket_server.py
+++ src/pyramid/services/socket_server.py
@@ -21,6 +21,7 @@ class SocketServerService(ISocketServerService, ServiceInjector):
 		self.__port = self.__common.port
 		self.is_running = False
 		self.server: sock | None = None
+		self.connected_clients: set[sock] = set()
 
 	def injectService(self,
 			logger_service: ILoggerService
@@ -28,16 +29,22 @@ class SocketServerService(ISocketServerService, ServiceInjector):
 		self.__logger = logger_service
 
 	async def open(self):
+		if self.is_running:
+			self.__logger.warning("Socket server is already running.")
+			return
 		self.server = sock(socket.AF_INET, socket.SOCK_STREAM)
 		self.server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 		self.server.bind((self.__host, self.__port))
 		self.server.listen(10)
 		self.server.setblocking(False)
-
-		self.__logger.info("Socket server open on %s:%d", self.__host, self.__port)
-
 		self.is_running = True
 
+		self.__logger.info("Socket server open on %s:%d", self.__host, self.__port)
+		await self.__run()
+
+	async def __run(self):
+		if not self.server:
+			return
 		while self.is_running:
 			client_socket: sock | None = None
 			client_address: Any = None
@@ -46,7 +53,10 @@ class SocketServerService(ISocketServerService, ServiceInjector):
 			response_to_send: SocketResponse | None = None
 			response_json: str | None = None
 			readable: list[sock]
+			# readable, writable, exceptional = select.select([self.server, *self.connected_clients], [], [], None)
+			self.__logger.debug("server.select")
 			readable, writable, exceptional = select.select([self.server], [], [], None)
+			self.__logger.debug("server.selected")
 
 			if not self.is_socket_open(self.server):
 				self.__logger.info("Socket server queue closed, stopping...")
@@ -54,18 +64,15 @@ class SocketServerService(ISocketServerService, ServiceInjector):
 	
 			if self.server in readable:
 				try:
+					self.__logger.debug("server.accept")
 					client_socket, client_address = self.server.accept()
+					self.__logger.debug("server.accepted")
 					client_socket.setblocking(False)
 					client_ip, client_port = client_address
+					self.connected_clients.add(client_socket)
+					self.__logger.debug("Client %s:%d connected", *client_address)
 
-					response_to_send = await self.__handle_client(client_socket, client_ip, client_port)
-					if response_to_send:
-						response_json = SocketCommon.serialize(
-							response_to_send.to_json(SocketCommon.serialize)
-						)
-						# Send the JSON response back to the client
-						# self.__logger.debug("[%s:%d] <- %s", client_ip, client_port, response_json)
-						self.__common.send_chunk(client_socket, response_json)
+					await self.__handle_client(client_socket, client_ip, client_port)
 				except Exception as err:
 					if isinstance(err, OSError) and err.errno == 9:
 						self.__logger.warning("Socket: [Errno 9] Bad file descriptor")
@@ -75,6 +82,8 @@ class SocketServerService(ISocketServerService, ServiceInjector):
 						raise err
 				finally:
 					if client_socket is not None:
+						self.__logger.debug("Client %s:%d disconnected ", *client_address)
+						self.connected_clients.discard(client_socket)
 						client_socket.close()
 		self.__logger.info("Socket server closed")
 
@@ -87,20 +96,45 @@ class SocketServerService(ISocketServerService, ServiceInjector):
 		try:
 			self.server.shutdown(socket.SHUT_RDWR)
 		except OSError as err:
-			if err.errno != 9:
-				self.__logger.warning("Error during socket shutdown: %s", err)
+			if err.errno == 9:
+				self.__logger.warning("Socket server already closed (Errno 9).")
 			else:
-				raise err
-		self.server.close()
-		self.__logger.info("Socket server stop")
-
-	async def __handle_client(self, client_socket: sock, client_ip, client_port) -> SocketResponse | None:
-		data = self.__common.receive_chunk(client_socket)
-
-		if not data:
-			self.__logger.info("[%s:%d] -> <empty>", client_ip, client_port)
+				self.__logger.warning("Error during socket shutdown: %s", err)
 			return
+		self.server.close()
 
+		for client in list(self.connected_clients):
+			try:
+				client.close()
+			except Exception:
+				pass
+		self.connected_clients.clear()
+		self.__logger.info("Socket server stopped")
+
+	async def __handle_client(self, client_socket: sock, client_ip, client_port):
+		try:
+			data = self.__common.receive_chunk(client_socket)
+
+			if not data:
+				self.__logger.info("[%s:%d] -> <empty>", client_ip, client_port)
+				self.connected_clients.remove(client_socket)
+				client_socket.close()
+				return
+			response = await self.__process_request(data, client_ip, client_port)
+			if response:
+				response_json = SocketCommon.serialize(
+					response.to_json(SocketCommon.serialize)
+				)
+				# Send the JSON response back to the client
+				# self.__logger.debug("[%s:%d] <- %s", client_ip, client_port, response_json)
+				self.__common.send_chunk(client_socket, response_json)
+
+		except Exception as err:
+			self.__logger.warning("Error handling client: %s", err)
+			self.connected_clients.discard(client_socket)
+			client_socket.close()
+
+	async def __process_request(self, data: str, client_ip, client_port) -> SocketResponse | None:
 		def object_hook(json):
 			if isinstance(json, dict):
 				return AskRequest(**json)
@@ -115,15 +149,13 @@ class SocketServerService(ISocketServerService, ServiceInjector):
 			return response
 
 		if json_data.action == "health":
-			data = PingSocket(True)
-			response.create(ResponseCode.OK, None, data)
+			pingSocket = PingSocket(True)
+			response.create(ResponseCode.OK, None, pingSocket)
 			return response
-
 		response.create(ResponseCode.ERROR, "Unknown action")
 		self.__logger.info(
 			"[%s:%d] <- Unknown action '%s'", client_ip, client_port, json_data.action
 		)
-		return response
 
 	@classmethod
 	def is_socket_open(cls, sock: sock):
